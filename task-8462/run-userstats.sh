ee98a7153f52818e41d8a4db9f20a2ab2306b582
Karsten Loesing <karsten.loesing@gmx.net>
2014-01-22 09:24:48 +0100

    Exclude microdescs from rsync (#8462).

--- task-8462/run-userstats.sh
+++ task-8462/run-userstats.sh
@@ -2,7 +2,7 @@
 set -e
 echo `date` "Starting."
 echo `date` "Downloading descriptors."
-rsync -arz --delete --exclude 'relay-descriptors/votes' metrics.torproject.org::metrics-recent in
+rsync -arz --delete --exclude 'relay-descriptors/votes' --exclude 'relay-descriptors/microdescs' metrics.torproject.org::metrics-recent in
 echo `date` "Parsing descriptors."
 javac -d bin/ -cp lib/commons-codec-1.6.jar:lib/commons-compress-1.4.1.jar:lib/descriptor.jar src/Parse.java
 java -cp bin/:lib/commons-codec-1.6.jar:lib/commons-compress-1.4.1.jar:lib/descriptor.jar Parse
